3dbd1b51b51d09ad7d97f4666b17d694a5bcd64c
eduardo <eduloni3@gmail.com>
2019-08-20 08:09:38 +0100

    fetch data

---
+++ utils/tmdb.js
@@ -0,0 +1,24 @@
+const key = '6374c3f94dec7aa26ae4c1b3ee43d4d7'
+const baseUrl = 'https://api.themoviedb.org/3'
+
+// search movie
+// `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}`
+
+// for details
+// `https://api.themoviedb.org/3/movie/${id}?api_key=${key}`
+
+// types can be movie, series, etc
+export const searchMovies = async (query, page, type = 'movie') => {
+  const url = `${baseUrl}/search/${type}?api_key=${key}&query=${query}&page=${page}`
+  const data = await fetch(url)
+  const json = await data.json()
+  return json
+}
+
+export const getDetails = async (id, type = 'movie') => {
+  const url = `${baseUrl}/${type}/${id}?api_key=${key}&append_to_response=credits`
+  const data = await fetch(url)
+  const json = await data.json()
+
+  return json
+}
